812178dcd39bd24b0396d54195b29b5402d76c19
dmitry <radah1823@gmail.com>
2024-09-01 19:53:38 +0300

    reqursive inst freq norm sinc tokenizer

---
+++ examples/tokenizers/inst_freq_norm_sinc_req.cpp
@@ -0,0 +1,36 @@
+#include <modes_extractors.hpp>
+//#include <cstdlib>
+#include <iostream>
+#include <tokenizer.hpp>
+
+int main(){
+    NP_DSP::ONE_D::GenericSignal<NP_DSP::ONE_D::SimpleVecWrapper<double>, true> data;
+    NP_DSP::ONE_D::GenericSignal<NP_DSP::ONE_D::SimpleVecWrapper<double>, true> aaa;
+
+    for(int i = 0; i < 500; i++) {
+        data.base->vec->push_back(std::rand());
+    }
+
+    NP_DSP::ONE_D::Tokenizers::InstFreqNormSincReqTokenizer tokenizer;
+    tokenizer.locality_coeff = 2.5;
+    tokenizer.period_muller = 1.05;
+    tokenizer.max_iter_number_for_filter = 3;
+    tokenizer.debug = false;
+
+
+    tokenizer.compute(data);
+
+    auto tokens = tokenizer.getTokens();
+
+    //IC(tokens[0]);
+    //IC(tokens);
+
+    for(const auto & token: tokens){
+        std::cout << token.mode_num << " " << token.t << " " <<
+            token.val << " " << token.inst_freq << " " <<
+            token.inst_ampl << " " << token.phase << std::endl;  
+    }
+
+    return 0;
+}
+
