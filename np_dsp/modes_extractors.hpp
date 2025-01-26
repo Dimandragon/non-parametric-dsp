ede020f71d459c802a02deb634e6479ff9daccee
Kuznetsov Dmitry <radah1823@gmail.com>
2025-01-27 00:18:55 +0300

    complete sotaemd struct

--- np_dsp/modes_extractors.hpp
+++ np_dsp/modes_extractors.hpp
@@ -2996,10 +2996,37 @@ struct instFreqNormSincReverseSolveNonDeterminity {
 };
 } // namespace EXPERIMENTAL
 
-enum class InstFreqType { HTBased, ExtremumsBased };
 enum class InstAmplKind { HTBased, ExtremumsBased };
 
 struct SOTAEMD {
+  NP_DSP::ONE_D::INST_FREQ_COMPUTERS::InstFreqComputingKind inst_freq_computing_kind = 
+    INST_FREQ_COMPUTERS::InstFreqComputingKind::ExtremumsBasedMultiquadric;
+
+  double max_iter_number_for_filter = 10;
+  NP_DSP::ONE_D::PHASE_SHIFTERS::RotateKind extremums_rotation_kind_e =
+      NP_DSP::ONE_D::PHASE_SHIFTERS::RotateKind::Naive;
+  double oversampling_ratio_for_ft_der = 1.0;
+
+  std::vector<double> phase_shifts{
+      0.0 * std::numbers::pi, 0.1 * std::numbers::pi, 0.2 * std::numbers::pi,
+      0.3 * std::numbers::pi, 0.4 * std::numbers::pi, 0.5 * std::numbers::pi,
+      0.6 * std::numbers::pi, 0.7 * std::numbers::pi, 0.8 * std::numbers::pi,
+      0.9 * std::numbers::pi};
+
+  int idw_layers = 15;
+  double idw_search_radius = 100;
+
+  double rbf_r_base = 20;
+  double rbf_n_layers = 20;
+  double rbf_lambda_n_s = 0.0;
+  double rbf_search_r = 0.4;
+  bool rbf_v3tol = true;
+
+  double rbf_lambda_v = 0.0;
+
+  double rbf_alpha = 10.0;
+  FILTERS::InterpolationKind interpolation_kind = FILTERS::InterpolationKind::Makima;
+
   using DataType = GenericSignal<SimpleVecWrapper<double>, true>;
   DataType data;
   DataType data_buffer;
@@ -3011,11 +3038,6 @@ struct SOTAEMD {
   std::vector<DataType *> phases;
   std::vector<double> freq_conv;
   std::vector<double> freq_conv_image;
-
-  double max_iter_number_for_filter = 10;
-  NP_DSP::ONE_D::PHASE_SHIFTERS::RotateKind extremums_rotation_kind_e =
-      NP_DSP::ONE_D::PHASE_SHIFTERS::RotateKind::Naive;
-  double oversampling_ratio_for_ft_der = 1.0;
 
   bool debug = false;
 
@@ -3031,41 +3053,22 @@ struct SOTAEMD {
       double, PHASE_COMPUTERS::ExtremumsKind::Simple, decltype(derivator)>
       phase_computer_simple;
 
-  INST_FREQ_COMPUTERS::ComputedOnPhase<
+  /*INST_FREQ_COMPUTERS::ComputedOnPhase<
       double, decltype(integrator), decltype(derivator),
       INST_FREQ_COMPUTERS::InstFreqDerivativeBasedKind::TimeAverage>
       inst_freq_computer = INST_FREQ_COMPUTERS::ComputedOnPhase<
           double, decltype(integrator), decltype(derivator),
           INST_FREQ_COMPUTERS::InstFreqDerivativeBasedKind::TimeAverage>(
-          integrator, derivator);
+          integrator, derivator);*/
+
+  INST_FREQ_COMPUTERS::SOTAInstFreqComputer inst_freq_computer;
 
   INST_AMPL_COMPUTERS::HilbertTransformBased<UTILITY_MATH::HTKind::Mull>
       inst_ampl_computer;
 
-  std::vector<double> phase_shifts{
-      0.0 * std::numbers::pi, 0.1 * std::numbers::pi, 0.2 * std::numbers::pi,
-      0.3 * std::numbers::pi, 0.4 * std::numbers::pi, 0.5 * std::numbers::pi,
-      0.6 * std::numbers::pi, 0.7 * std::numbers::pi, 0.8 * std::numbers::pi,
-      0.9 * std::numbers::pi};
-
   FILTERS::RecursiveFilter<double,
                            FILTERS::LocalFilteringType::InterpolationExtremums>
       filter;
-
-  int idw_layers = 15;
-  double idw_search_radius = 100;
-
-  double rbf_r_base = 20;
-  double rbf_n_layers = 20;
-  double rbf_lambda_n_s = 0.0;
-  double rbf_search_r = 0.4;
-  bool rbf_v3tol = true;
-
-  double rbf_lambda_v = 0.0;
-
-  double rbf_alpha = 10.0;
-  FILTERS::InterpolationKind interpolation_kind =
-      FILTERS::InterpolationKind::Makima;
 
   template <Signal DataInT> void load(const DataInT &data_in) {
     data.base->vec->clear();
@@ -3086,7 +3089,27 @@ struct SOTAEMD {
     }
   }
 
+  void setPhaseShiftsByCount(int count, double max_shift, bool including_max_shift){
+    if (count == 0){
+      phase_shifts = {0.0};
+    }
+    else{
+      double d_phase_shift = max_shift / (count);
+      if (including_max_shift){
+        if (count != 1){
+          d_phase_shift = max_shift / (count + 1);
+        }
+      }
+      phase_shifts.clear();
+      for (int i = 0; i < count; i++){
+        phase_shifts.push_back(i * d_phase_shift);
+      }
+    }
+    //todo
+  }
+
   template <typename DataT> void compute(const DataT &data_in) {
+    inst_freq_computer.kind = inst_freq_computing_kind;
     filter.filter.idw_layers = idw_layers;
     filter.filter.idw_search_radius = idw_search_radius;
     filter.filter.rbf_r_base = rbf_r_base;
@@ -3099,6 +3122,12 @@ struct SOTAEMD {
     filter.filter.interpolation_kind = interpolation_kind;
 
     filter.filter.phase_shifts = phase_shifts;
+
+    auto phase_shifts_local = phase_shifts;
+    //IC(phase_shifts.size());
+    //for (int i = 0; i < phase_shifts.size(); i++){
+    //  IC(phase_shifts[i]);
+    //}
     // filter.inst_freq_computer = &inst_freq_computer;
     // filter.phase_computer = &phase_computer_simple;
     filter.max_iters = max_iter_number_for_filter;
@@ -3112,6 +3141,8 @@ struct SOTAEMD {
 
     DataType non_resampled_data;
 
+    
+
     auto prepare_memory_ext = [&]() {
       for (int i = 0; i < data_in.size(); i++) {
         non_resampled_data.base->vec->push_back(data_in[i]);
@@ -3120,6 +3151,11 @@ struct SOTAEMD {
         data.base->vec->clear();
         for (int i = 0; i < data_in.size(); i++) {
           data.base->vec->push_back(data_in[i]);
+        }
+      }
+      else{
+        for (int i = 0; i < data_in.size(); i++) {
+          data[i] = data_in[i];
         }
       }
       if (data_buffer.size() != data_in.size()) {
@@ -3218,6 +3254,7 @@ struct SOTAEMD {
       phase_computer_simple.compute(data, *phases[iter_number], nullptr);
 
       if (debug) {
+        //data.show(PlottingKind::Simple);
         std::cout << "compute first phase  " << iter_number << std::endl;
         phases[iter_number]->show(PlottingKind::Simple);
       }
@@ -3252,7 +3289,7 @@ struct SOTAEMD {
           phases[iter_number]->show(PlottingKind::Simple); //, label.str());
         }
 
-        inst_freq_computer.compute(*phases[iter_number],
+        inst_freq_computer.compute(*modes[iter_number],
                                    *inst_freqs[iter_number], nullptr);
 
         if (debug) {
@@ -3278,11 +3315,35 @@ struct SOTAEMD {
         // std::endl; modes[iter_number]->show(PlottingKind::Simple);
         phase_computer_simple.compute(*modes[iter_number], *phases[iter_number],
                                       nullptr);
-        inst_freq_computer.compute(*phases[iter_number],
+        inst_freq_computer.compute(*modes[iter_number],
                                    *inst_freqs[iter_number], nullptr);
         inst_ampl_computer.compute(*modes[iter_number],
                                    *inst_ampls[iter_number], nullptr);
 
+        for(int i = iter_number + 1; i < modes.size(); i++){
+          delete modes[i];
+        }
+        for(int i = iter_number + 1; i < phases.size(); i++){
+          delete phases[i];
+        }
+        for(int i = iter_number + 1; i < inst_freqs.size(); i++){
+          delete inst_freqs[i];
+        }
+        for(int i = iter_number + 1; i < inst_ampls.size(); i++){
+          delete inst_ampls[i];
+        }
+        while (modes.size() - 1 > iter_number){
+          modes.pop_back();
+        }
+        while (phases.size() - 1 > iter_number){
+          phases.pop_back();
+        }
+        while (inst_ampls.size() - 1 > iter_number){
+          inst_ampls.pop_back();
+        }
+        while (inst_freqs.size() - 1 > iter_number){
+          inst_freqs.pop_back();
+        }
         iter_number++;
         return;
       }
@@ -3290,6 +3351,7 @@ struct SOTAEMD {
   }
 
   int getModesCount() const { return static_cast<int>(modes.size()); }
+
   int getDataSize() const { return static_cast<int>(modes[0]->size()); }
   std::vector<double> getMode(int idx) const {
     return *(modes[idx]->base->vec);
@@ -3302,6 +3364,25 @@ struct SOTAEMD {
   }
   std::vector<double> getPhase(int idx) const {
     return *(phases[idx]->base->vec);
+  }
+
+  ~SOTAEMD(){
+    for (auto pointer: modes){
+      delete pointer;
+    }
+    modes.clear();
+    for (auto pointer: inst_freqs){
+      delete pointer;
+    }
+    inst_freqs.clear();
+    for (auto pointer: inst_ampls){
+      delete pointer;
+    }
+    inst_ampls.clear();
+    for (auto pointer: phases){
+      delete pointer;
+    }
+    phases.clear();
   }
 };
 } // namespace NP_DSP::ONE_D::MODES_EXTRACTORS
